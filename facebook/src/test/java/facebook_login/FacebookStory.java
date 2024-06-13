eb3310340e5bd2c90e4709804de6341a41e97acd
hp <hp@192.168.1.186>
2024-06-13 09:20:10 +0530

    story

---
+++ facebook/src/test/java/facebook_login/FacebookStory.java
@@ -0,0 +1,16 @@
+package facebook_login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class FacebookStory 
+{
+	
+	@Test
+	
+	public void story()
+	{
+		Reporter.log("facebook story is executed",true);
+	}
+
+}
