7432eacc42ef76092dd72e291fee47ecdf576716
hp <hp@192.168.1.186>
2024-06-13 09:12:08 +0530

    commited

---
+++ facebook/src/test/java/facebook_login/LoginTest.java
@@ -0,0 +1,16 @@
+package facebook_login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LoginTest 
+{
+	@Test
+	
+	public void login()
+	{
+		Reporter.log("login is executed",true);
+	}
+	
+ 
+}
---
+++ facebook/src/test/java/facebook_login/SignupTest.java
@@ -0,0 +1,16 @@
+package facebook_login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SignupTest 
+{
+	
+	  @Test
+	  
+	  public void signup()
+	  {
+		  Reporter.log("signup is executed",true);
+	  }
+
+}
